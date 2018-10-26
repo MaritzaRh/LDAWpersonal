a00a896c73ba22b9361a8abec21ff79844d0838a
MaritzaVIDEOJUEGOS <A01701357@itesm.mx>
2018-10-26 18:17:47 -0500

    Fix problems

--- environment/sample_app/sample_app/test/mailers/user_mailer_test.rb
+++ environment/sample_app/sample_app/test/mailers/user_mailer_test.rb
@@ -1,16 +1,17 @@
 require 'test_helper'
 
-class UserMailerTest < ActionMailer::TestCase
+class UserMailerPreview < ActionMailer::Preview
 
-  test "account_activation" do
-    user = users(:michael)
+  # Preview this email at http://localhost:3000/rails/mailers/user_mailer/account_activation
+  def account_activation(user)
+    user = User.first
     user.activation_token = User.new_token
-    mail = UserMailer.account_activation(user)
-    assert_equal "Account activation", mail.subject
-    assert_equal [user.email], mail.to
-    assert_equal ["noreply@example.com"], mail.from
-    assert_match user.name,               mail.body.encoded
-    assert_match user.activation_token,   mail.body.encoded
-    assert_match CGI.escape(user.email),  mail.body.encoded
+    UserMailer.account_activation(user)
   end
+
+  # Preview this email at http://localhost:3000/rails/mailers/user_mailer/password_reset
+  def password_reset
+    UserMailer.password_reset
+  end
+
 end
